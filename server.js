614c6ab1c8a02974c4ae8485a26fd1742fdc96b5
Stephen Thompson <smt3@hotmail.com>
2019-09-23 20:36:59 -0500

    bad code test TRAVIS

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+reque("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+ar exphbs = require("express-handlebars");
 
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT |-| 3000
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
