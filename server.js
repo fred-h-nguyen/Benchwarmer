a264e5739f29b2beffd963b5d6194b39aea378fd
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-09-26 16:44:01 -0500

    required the necessary packages for passport and initialize passport

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
+//Requiring npm packages
 var express = require("express");
 var exphbs = require("express-handlebars");
+var session = require("express-session");
+//Requiring passport
+var passport = require("./config/passport");
 
 var db = require("./models");
 
@@ -11,6 +15,17 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+//session set up to keep track of user session
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
