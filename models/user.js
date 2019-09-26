36f3d45925dcf7844218d573fdc633dfe33e44a1
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-09-26 15:40:18 -0500

    created the sequelize table for the user with password hashing

---
+++ models/user.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("Users", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  //Method for checking unhashed password with hashed one for validation
+  User.prototype.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  //Hook so that before the user is created, the password will be hashed with the middleware
+
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+
+  return User;
+};
