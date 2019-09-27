72fc7e522d81fdd077feb55374766f1462131f09
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-09-27 01:03:43 -0500

    add passport api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,35 @@
+//required models and passport for api routes
 var db = require("../models");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
   // Get all examples
+
+  // passport login post
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    res.json(req.user);
+  });
+
+  //Passport sign-up post
+  app.post("/api/signup", function(req, res) {
+    db.User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
+  });
+
+  //Logout get route
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
