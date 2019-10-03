4e6533a353f2fe561a9a1dda586dec7970997f14
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-10-02 19:44:08 -0500

    prettify

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,36 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
+  app.get("/", function(req, res) {
     var user = req.user;
     if (user) {
-      db.fbRoster.findAll({ where: { ownerId: user.id } }).then(function (roster) {
-        res.render("index", {
-          user: user,
-          roster: roster
+      db.fbRoster
+        .findAll({ where: { ownerId: user.id } })
+        .then(function(roster) {
+          res.render("index", {
+            user: user,
+            roster: roster
+          });
         });
-      });
-    }else{
-      res.render("index",{
-        user:user
+    } else {
+      res.render("index", {
+        user: user
       });
     }
   });
 
   //Load signup page
-  app.get("/signup", function (req, res) {
+  app.get("/signup", function(req, res) {
     res.render("signup", {
       logged: false
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/roster", function (req, res) {
+  app.get("/roster", function(req, res) {
     var logged = req.user;
-    db.fbRoster.findAll({}).then(function (roster) {
+    db.fbRoster.findAll({}).then(function(roster) {
       res.render("index", {
         logged: logged,
         roster: roster
@@ -37,7 +39,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
